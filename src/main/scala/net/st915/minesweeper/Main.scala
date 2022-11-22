2a09db84f8899a7e04ae9dd77b416ab2e0a76f19
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-22 11:14:51 +0900

    Add AboutPage

--- src/main/scala/net/st915/minesweeper/Main.scala
+++ src/main/scala/net/st915/minesweeper/Main.scala
@@ -2,7 +2,7 @@ package net.st915.minesweeper
 
 import cats.effect.IO
 import net.st915.minesweeper.implicits.*
-import org.scalajs.dom.{Document, document}
+import org.scalajs.dom.{Document, HTMLLinkElement, document}
 
 import scala.util.chaining.*
 
@@ -22,9 +22,54 @@ import scala.util.chaining.*
       .tap(document.body.appendChild)
   }
 
+  val appendAboutPage = IO {
+    document.createElement("div")
+      .tap(div =>
+        document.createElement("p")
+          .tap(_.appendChild("This site is licensed under the ".textNode))
+          .tap(p =>
+            document.createElement("a")
+              .asInstanceOf[HTMLLinkElement]
+              .tap(_.appendChild("MIT License".textNode))
+              .tap(_.href =
+                "https://github.com/stouma915/minesweeper-scala/blob/main/LICENSE"
+              )
+              .tap(p.appendChild)
+          )
+          .tap(_.appendChild(".".textNode))
+          .tap(_.appendChild(document.createElement("br")))
+          .tap(_.appendChild("This site is open source. ".textNode))
+          .tap(p =>
+            document.createElement("a")
+              .asInstanceOf[HTMLLinkElement]
+              .tap(_.appendChild("Improve this site".textNode))
+              .tap(_.href =
+                "https://github.com/stouma915/minesweeper-scala"
+              )
+              .tap(p.appendChild)
+          )
+          .tap(_.appendChild(".".textNode))
+          .tap(_.appendChild(document.createElement("br")))
+          .tap(_.appendChild("Powered by ".textNode))
+          .tap(p =>
+            document.createElement("a")
+              .asInstanceOf[HTMLLinkElement]
+              .tap(_.appendChild("GitHub Pages".textNode))
+              .tap(_.href =
+                "https://pages.github.com"
+              )
+              .tap(p.appendChild)
+          )
+          .tap(_.appendChild(".".textNode))
+          .tap(div.appendChild)
+      )
+      .tap(document.body.appendChild)
+  }
+
   val task = for {
     _ <- printTestMessage
     _ <- appendTestMessage
+    _ <- appendAboutPage
   } yield ()
 
   task.unsafeRunAndForget()
