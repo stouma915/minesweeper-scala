1b3789387ec8201c9b20047d19fb3935d55b85e2
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-22 13:27:00 +0900

    Add Difficulty.scala

---
+++ src/main/scala/net/st915/minesweeper/Difficulty.scala
@@ -0,0 +1,46 @@
+package net.st915.minesweeper
+
+object Difficulty {
+
+  val Easy = Difficulty(
+    "easy",
+    9,
+    9,
+    10
+  )
+  val Normal = Difficulty(
+    "normal",
+    16,
+    16,
+    40
+  )
+  val Hard = Difficulty(
+    "hard",
+    30,
+    16,
+    99
+  )
+  val Impossible = Difficulty(
+    "impossible",
+    9,
+    9,
+    67
+  )
+
+  val Difficulties = Seq(
+    Easy,
+    Normal,
+    Hard,
+    Impossible
+  )
+
+  val Default = Easy
+
+}
+
+case class Difficulty(
+  name: String,
+  width: Int,
+  height: Int,
+  mines: Int
+)
