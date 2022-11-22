8667609d5bbd34b2deab7e4a56b5ab401b797efe
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-22 19:11:56 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+SCALA_VERSION=3.2.1
+
 mkdir -p out
 
 cp -f index.html out
 cp -rf resources out
 
-sbt fastLinkJS
-
-cp -f target/scala-*/minesweeper-scala-fastopt/main.js out
+if [ "$1" = "production" ]; then
+	echo "This is Production Build."
+	sbt fullLinkJS
+	cp -f target/scala-$SCALA_VERSION/minesweeper-scala-opt/main.js out
+else
+	echo "This is Debug Build."
+	sbt fastLinkJS
+	cp -f target/scala-$SCALA_VERSION/minesweeper-scala-fastopt/main.js out
+fi
