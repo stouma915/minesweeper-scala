ef86e00a5212193a8443be6eea29ce6d288a5053
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-23 16:31:39 +0900

    Add cell click event handler

--- src/main/scala/net/st915/minesweeper/component/GameScreen.scala
+++ src/main/scala/net/st915/minesweeper/component/GameScreen.scala
@@ -1,29 +1,54 @@
 package net.st915.minesweeper.component
 
 import cats.effect.IO
+import cats.effect.unsafe.IORuntime
+import net.st915.minesweeper.Coordinate
 import net.st915.minesweeper.difficulty.Difficulty
 import net.st915.minesweeper.implicits.*
-import org.scalajs.dom.{Document, Element}
+import org.scalajs.dom.{Document, Element, HTMLDivElement, MouseEvent}
 
 import scala.util.chaining.*
 
 object GameScreen {
 
-  def make(difficulty: Difficulty)(implicit doc: Document): IO[Element] = IO {
+  def onClick(implicit event: MouseEvent, coord: Coordinate): IO[Unit] =
+    IO {
+      println(s"CLICK: (${coord.x}, ${coord.y})")
+    }
+
+  def onRightClick(implicit event: MouseEvent, coord: Coordinate): IO[Unit] =
+    IO {
+      println(s"RIGHT CLICK: (${coord.x}, ${coord.y})")
+    }
+
+  def make(
+      difficulty: Difficulty
+  )(implicit doc: Document, runtime: IORuntime): IO[Element] = IO {
     doc
       .createElement("div")
       .tap(_.classList.add("gameScreen"))
       .tap { div =>
-        (0 until difficulty.height).foreach { line =>
+        (0 until difficulty.height).foreach { y =>
           doc
             .createElement("div")
             .tap(_.classList.add("line"))
             .tap { lineDiv =>
-              (0 until difficulty.width).foreach { cell =>
+              (0 until difficulty.width).foreach { x =>
+                implicit val _coord: Coordinate = Coordinate(x, y)
+
                 doc
                   .createElement("div")
+                  .asInstanceOf[HTMLDivElement]
                   .tap(_.classList.add("cellNotOpened"))
-                  .tap(_.id = s"${line}_$cell")
+                  .tap(_.id = s"${x}_$y")
+                  .tap(_.onclick = implicit e => {
+                    e.preventDefault()
+                    onClick.unsafeRunAndForget()
+                  })
+                  .tap(_.oncontextmenu = implicit e => {
+                    e.preventDefault()
+                    onRightClick.unsafeRunAndForget()
+                  })
                   .tap(lineDiv.appendChild)
               }
             }
