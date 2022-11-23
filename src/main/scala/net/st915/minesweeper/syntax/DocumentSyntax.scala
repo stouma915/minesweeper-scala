ff029af9c3ac826cfb385c46a6c27121d0f76bda
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-11-24 07:38:18 +0900

    Create DocumentSyntax.scala

---
+++ src/main/scala/net/st915/minesweeper/syntax/DocumentSyntax.scala
@@ -0,0 +1,16 @@
+package net.st915.minesweeper.syntax
+
+import org.scalajs.dom.Document
+
+trait DocumentSyntax {
+
+  implicit class DocumentOps(doc: Document) {
+
+    def createElementWithType[A](name: String): A =
+      doc
+        .createElement(name)
+        .asInstaceOf[A]
+
+  }
+
+}
