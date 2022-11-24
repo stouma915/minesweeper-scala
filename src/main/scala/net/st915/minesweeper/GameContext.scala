aac0a7f14ce3f47811494a9270ef5e192111289c
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-24 17:32:44 +0900

    Update GameContext.scala

--- src/main/scala/net/st915/minesweeper/GameContext.scala
+++ src/main/scala/net/st915/minesweeper/GameContext.scala
@@ -4,38 +4,60 @@ import net.st915.minesweeper.Coordinate
 
 object GameContext {
 
-  def empty: GameContext = GameContext(
-    false,
-    false,
-    List()
-  )
+  def empty: GameContext = GameContext()
 
 }
 
 case class GameContext(
-    gameStarted: Boolean,
-    gameEnded: Boolean,
-    opened: List[Coordinate]
+    gameStarted: Boolean = false,
+    gameEnded: Boolean = false,
+    opened: List[Coordinate] = List(),
+    mines: List[Coordinate] = List(),
+    flagged: List[Coordinate] = List()
 ) {
 
   def updateGameStarted(bool: Boolean): GameContext =
     GameContext(
       bool,
       gameEnded,
-      opened
+      opened,
+      mines,
+      flagged
     )
 
   def updateGameEnded(bool: Boolean): GameContext =
     GameContext(
       gameStarted,
       bool,
-      opened
+      opened,
+      mines,
+      flagged
     )
 
   def updateOpened(list: List[Coordinate]): GameContext =
     GameContext(
       gameStarted,
       gameEnded,
+      list,
+      mines,
+      flagged
+    )
+
+  def updateMines(list: List[Coordinate]): GameContext =
+    GameContext(
+      gameStarted,
+      gameEnded,
+      opened,
+      list,
+      flagged
+    )
+
+  def updateFlagged(list: List[Coordinate]): GameContext =
+    GameContext(
+      gameStarted,
+      gameEnded,
+      opened,
+      mines,
       list
     )
 
