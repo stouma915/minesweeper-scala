4edc93c9a01b8a6c75024120248864f7b9e21068
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-28 15:11:36 +0900

    Add Util.scala

---
+++ src/main/scala/net/st915/minesweeper/Util.scala
@@ -0,0 +1,23 @@
+package net.st915.minesweeper
+
+import cats.effect.IO
+import cats.effect.unsafe.IORuntime
+import net.st915.minesweeper.Coordinate
+import net.st915.minesweeper.difficulty.Difficulty
+
+object Util {
+
+  def eachCoord(
+    difficulty: Difficulty,
+    program: Coordinate => IO[Unit]
+  )(implicit runtime: IORuntime): IO[Unit] = IO {
+    (0 until difficulty.height).foreach { y =>
+      (0 until difficulty.width).foreach { x =>
+        val coord = Coordinate(x, y)
+
+        program(coord).unsafeRunAndForget()
+      }
+    }
+  }
+
+}
