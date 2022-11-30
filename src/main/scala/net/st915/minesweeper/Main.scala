94824489c61c538d6375ea5866bf26603660dda2
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-30 11:51:01 +0900

    Remove UnknownDifficulty.scala and Add RenderingScreen.scala

--- src/main/scala/net/st915/minesweeper/Main.scala
+++ src/main/scala/net/st915/minesweeper/Main.scala
@@ -1,6 +1,5 @@
 package net.st915.minesweeper
 
-import cats.data.OptionT
 import cats.effect.IO
 import cats.effect.unsafe.IORuntime
 import net.st915.minesweeper.component.*
@@ -15,35 +14,35 @@ import org.scalajs.dom.*
 
   val appendToBody: Element => IO[Unit] = e => IO(document.body.appendChild(e))
 
-  val getDifficulty: IO[Option[Difficulty]] = IO {
+  val getDifficulty: IO[Difficulty] = IO {
     val params = new URLSearchParams(window.location.search)
-    if (params.has("d")) Difficulties.All.find(_.id eq params.get("d"))
-    else Some(Difficulties.Default)
+    if (params.has("d"))
+      Difficulties.All.find(_.id eq params.get("d")) match {
+        case Some(d) => d
+        case None    => Difficulties.Default
+      }
+    else
+      Difficulties.Default
   }
 
-  import scala.language.implicitConversions
-  implicit def ioAToIOOptA[A](io: IO[A]): IO[Option[A]] = io.map(x => Some(x))
-  def optT[A](io: IO[Option[A]]): OptionT[IO, A] = OptionT(io)
-  def ioSome[A](a: A): IO[Option[A]] = IO(Some(a))
-
   val renderGame = for {
-    unknownDiff <- optT(UnknownDifficulty.make)
-    _ <- optT(appendToBody(unknownDiff))
-    difficulty <- optT(getDifficulty)
-    gameScreen <- optT(GameScreen.make(difficulty))
-    diffSelect <- optT(DifficultySelector.make)
-    _ <- optT(ioSome(unknownDiff.remove()))
-    _ <- optT(appendToBody(gameScreen))
-    _ <- optT(appendToBody(diffSelect))
-    gameLogic <- optT(ioSome(GameLogic(difficulty)))
-    docUpdater <- optT(ioSome(DocumentUpdater(difficulty)))
-    _ <- optT(MainLoop(gameLogic, docUpdater).startMainLoop)
+    renderingScreen <- RenderingScreen.make
+    _ <- appendToBody(renderingScreen)
+    difficulty <- getDifficulty
+    gameScreen <- GameScreen.make(difficulty)
+    diffSelect <- DifficultySelector.make
+    _ <- IO(renderingScreen.remove())
+    _ <- appendToBody(gameScreen)
+    _ <- appendToBody(diffSelect)
+    gameLogic <- IO(GameLogic(difficulty))
+    docUpdater <- IO(DocumentUpdater(difficulty))
+    _ <- MainLoop(gameLogic, docUpdater).startMainLoop
   } yield ()
 
   val program = for {
     infoText <- InformationText.make
     _ <- appendToBody(infoText)
-    _ <- renderGame.value
+    _ <- renderGame
     aboutPage <- AboutPage.make
     _ <- appendToBody(aboutPage)
   } yield ()
