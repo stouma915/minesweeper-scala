0a8dc44b5732d171a20b039a0e6af996f23a4fab
stouma915 <sasakitouma@yahoo.ne.jp>
2022-11-30 11:25:36 +0900

    Refactor Util.scala

--- src/main/scala/net/st915/minesweeper/Util.scala
+++ src/main/scala/net/st915/minesweeper/Util.scala
@@ -6,18 +6,39 @@ import net.st915.minesweeper.Coordinate
 
 object Util {
 
-  def forAllCoords(
-      difficulty: Difficulty,
-      program: Coordinate => IO[_]
-  )(implicit runtime: IORuntime): IO[Unit] = IO {
-    (0 until difficulty.width).foreach { x =>
-      (0 until difficulty.height).foreach { y =>
-        val coord = Coordinate(x, y)
+  import cats.implicits.*
+  import cats.syntax.parallel.*
 
-        program(coord).unsafeRunAndForget()
+  def makeListOfIO[A](
+      difficulty: Difficulty,
+      program: Coordinate => IO[A]
+  ): List[IO[A]] =
+    (0 until difficulty.width)
+      .map { x =>
+        (0 until difficulty.height)
+          .map { y =>
+            program(Coordinate(x, y))
+          }
+          .toList
       }
-    }
-  }
+      .toList
+      .flatten
+
+  def forAllCoords[A](
+      difficulty: Difficulty,
+      program: Coordinate => IO[A]
+  )(implicit runtime: IORuntime): IO[Unit] =
+    for {
+      _ <- makeListOfIO[A](difficulty, program).sequence
+    } yield ()
+
+  def forAllCoordsPar[A](
+      difficulty: Difficulty,
+      program: Coordinate => IO[A]
+  )(implicit runtime: IORuntime): IO[Unit] =
+    for {
+      _ <- makeListOfIO[A](difficulty, program).parSequence
+    } yield ()
 
   def get3x3(
       coord: Coordinate,
