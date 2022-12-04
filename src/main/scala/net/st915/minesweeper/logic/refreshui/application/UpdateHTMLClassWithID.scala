54bdcbea7b694563b3c4da981b7ade04632b88f5
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-05 08:17:26 +0900

    Add UpdateHTMLClassWithID

---
+++ src/main/scala/net/st915/minesweeper/logic/refreshui/application/UpdateHTMLClassWithID.scala
@@ -0,0 +1,15 @@
+package net.st915.minesweeper.logic.refreshui.application
+
+import org.scalajs.dom.*
+
+object UpdateHTMLClassWithID {
+
+  def apply[F[_]: UpdateHTMLClassWithID]: UpdateHTMLClassWithID[F] = implicitly
+
+}
+
+trait UpdateHTMLClassWithID[F[_]] {
+
+  def update(id: String, htmlClass: String)(implicit document: HTMLDocument): F[Unit]
+
+}
---
+++ src/main/scala/net/st915/minesweeper/logic/refreshui/impl/SyncUpdateHTMLClassWithID.scala
@@ -0,0 +1,19 @@
+package net.st915.minesweeper.logic.refreshui.impl
+
+import cats.effect.Sync
+import net.st915.minesweeper.logic.refreshui.application.*
+import org.scalajs.dom.*
+
+class SyncUpdateHTMLClassWithID[F[_]: Sync: GetElement] extends UpdateHTMLClassWithID[F] {
+
+  import cats.syntax.flatMap.*
+
+  def update(id: String, htmlClass: String)(implicit document: HTMLDocument): F[Unit] =
+    GetElement[F].get(id) >>= { element =>
+      if (element.className != htmlClass)
+        Sync[F].blocking(element.className = htmlClass)
+      else
+        Sync[F].unit
+    }
+
+}
