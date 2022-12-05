b91db0c3f2bbc26c07d9f77c0442f43b629f7261
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-05 17:05:44 +0900

    Update OpenCellLogic

--- src/main/scala/net/st915/minesweeper/logic/OpenCellLogic.scala
+++ src/main/scala/net/st915/minesweeper/logic/OpenCellLogic.scala
@@ -10,12 +10,9 @@ object OpenCellLogic {
   def wired[F[_]: Sync](coord: Coordinate)(implicit gameState: GameState): F[GameState] = {
     implicit val _ifFlagged: IfFlagged[F] = ApplicativeIfFlagged[F]
     implicit val _ifOpened: IfOpened[F] = ApplicativeIfOpened[F]
-    implicit val _ifGameStarted: IfGameStarted[F] = ApplicativeIfGameStarted[F]
-    implicit val _ifGameStopped: IfGameStopped[F] = ApplicativeIfGameStopped[F]
 
     implicit val _doNothing: DoNothing[F] = ApplicativeDoNothing[F]
     implicit val _addOpenedCoord: AddOpenedCoord[F] = ApplicativeAddOpenedCoord[F]
-    implicit val _ifCanOpenOperation: IfCanOpenOperation[F] = ApplicativeIfCanOpenOperation[F]
     implicit val _ifNotOpenedAndNotFlagged: IfNotOpenedAndNotFlagged[F] =
       ApplicativeIfNotOpenedAndNotFlagged[F]
 
@@ -23,16 +20,11 @@ object OpenCellLogic {
   }
 
   def apply[
-    F[_]: Sync: IfCanOpenOperation: IfNotOpenedAndNotFlagged: AddOpenedCoord: DoNothing
+    F[_]: Sync: IfNotOpenedAndNotFlagged: AddOpenedCoord: DoNothing
   ](coord: Coordinate)(implicit gameState: GameState): F[GameState] =
-    IfCanOpenOperation[F].perform {
-      IfNotOpenedAndNotFlagged[F].perform(coord) {
-        AddOpenedCoord[F].add(coord)
-      } {
-        DoNothing[F].perform
-      }
+    IfNotOpenedAndNotFlagged[F].perform(coord) {
+      AddOpenedCoord[F].add(coord)
     } {
-      // TODO: Write start game logic here
       DoNothing[F].perform
     }
 
--- src/main/scala/net/st915/minesweeper/logic/eventhandler/HandleCellClickEvent.scala
+++ src/main/scala/net/st915/minesweeper/logic/eventhandler/HandleCellClickEvent.scala
@@ -4,23 +4,32 @@ import cats.effect.Sync
 import net.st915.minesweeper.GameState
 import net.st915.minesweeper.event.CellClickEvent
 import net.st915.minesweeper.logic.*
-import net.st915.minesweeper.logic.application.IfInFlagPlaceMode
-import net.st915.minesweeper.logic.impl.ApplicativeIfInFlagPlaceMode
+import net.st915.minesweeper.logic.application.*
+import net.st915.minesweeper.logic.impl.*
 
 object HandleCellClickEvent {
 
   def wired[F[_]: Sync](event: CellClickEvent)(implicit gameState: GameState): F[GameState] = {
+    implicit val _doNothing: DoNothing[F] = ApplicativeDoNothing[F]
+    implicit val _ifGameStarted: IfGameStarted[F] = ApplicativeIfGameStarted[F]
+    implicit val _ifGameStopped: IfGameStopped[F] = ApplicativeIfGameStopped[F]
+    implicit val _ifCanOpenOperation: IfCanOpenOperation[F] = ApplicativeIfCanOpenOperation[F]
     implicit val _ifInFlagPlaceMode: IfInFlagPlaceMode[F] = ApplicativeIfInFlagPlaceMode[F]
 
     HandleCellClickEvent(event)
   }
 
   def apply[
-    F[_]: Sync: IfInFlagPlaceMode
+    F[_]: Sync: IfCanOpenOperation: IfInFlagPlaceMode
   ](event: CellClickEvent)(implicit gameState: GameState): F[GameState] =
-    IfInFlagPlaceMode[F].perform {
-      PlaceFlagLogic.wired[F](event.coord)
+    IfCanOpenOperation[F].perform {
+      IfInFlagPlaceMode[F].perform {
+        PlaceFlagLogic.wired[F](event.coord)
+      } {
+        OpenCellLogic.wired[F](event.coord)
+      }
     } {
-      OpenCellLogic.wired[F](event.coord)
+      // TODO: Write start game logic here
+      Sync[F].pure(gameState)
     }
 }
