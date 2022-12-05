40a6aab910f61df9b8755b07e5a84e6f128da39c
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-05 16:48:08 +0900

    Update HandleCellClickEvent

--- src/main/scala/net/st915/minesweeper/logic/eventhandler/HandleCellClickEvent.scala
+++ src/main/scala/net/st915/minesweeper/logic/eventhandler/HandleCellClickEvent.scala
@@ -3,13 +3,24 @@ package net.st915.minesweeper.logic.eventhandler
 import cats.effect.Sync
 import net.st915.minesweeper.GameState
 import net.st915.minesweeper.event.CellClickEvent
-import net.st915.minesweeper.logic.OpenCellLogic
+import net.st915.minesweeper.logic.*
+import net.st915.minesweeper.logic.application.IfInFlagPlaceMode
+import net.st915.minesweeper.logic.impl.ApplicativeIfInFlagPlaceMode
 
 object HandleCellClickEvent {
 
-  def wired[F[_]: Sync](event: CellClickEvent)(implicit gameState: GameState): F[GameState] =
-    HandleCellClickEvent(event)
+  def wired[F[_]: Sync](event: CellClickEvent)(implicit gameState: GameState): F[GameState] = {
+    implicit val _ifInFlagPlaceMode: IfInFlagPlaceMode[F] = ApplicativeIfInFlagPlaceMode[F]
 
-  def apply[F[_]: Sync](event: CellClickEvent)(implicit gameState: GameState): F[GameState] =
-    OpenCellLogic.wired[F](event.coord)
+    HandleCellClickEvent(event)
+  }
+
+  def apply[
+    F[_]: Sync: IfInFlagPlaceMode
+  ](event: CellClickEvent)(implicit gameState: GameState): F[GameState] =
+    IfInFlagPlaceMode[F].perform {
+      PlaceFlagLogic.wired[F](event.coord)
+    } {
+      OpenCellLogic.wired[F](event.coord)
+    }
 }
