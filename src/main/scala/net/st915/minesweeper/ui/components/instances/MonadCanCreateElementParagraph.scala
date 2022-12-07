7a5e0297bc41c717a773c447e10ee84a25889ead
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-07 13:06:41 +0900

    Update MonadCanCreateElementParagraph.scala

--- src/main/scala/net/st915/minesweeper/ui/components/instances/MonadCanCreateElementParagraph.scala
+++ src/main/scala/net/st915/minesweeper/ui/components/instances/MonadCanCreateElementParagraph.scala
@@ -4,7 +4,8 @@ import cats.Monad
 import net.st915.minesweeper.ui.components.typeclasses.CanCreateElement
 import org.scalajs.dom.*
 
-class MonadCanCreateElementParagraph[F[_]: Monad] extends CanCreateElement[F, HTMLParagraphElement] {
+class MonadCanCreateElementParagraph[F[_]: Monad]
+    extends CanCreateElement[F, HTMLParagraphElement] {
 
   override def create(using HTMLDocument): F[HTMLParagraphElement] =
     Monad[F].pure {
