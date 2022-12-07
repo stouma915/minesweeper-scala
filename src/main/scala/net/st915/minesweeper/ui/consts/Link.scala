1a29b817668debe656e793ec2a75a1e2143d4fef
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-07 16:31:35 +0900

    Addd UI Constants

---
+++ src/main/scala/net/st915/minesweeper/ui/consts/Link.scala
@@ -0,0 +1,11 @@
+package net.st915.minesweeper.ui.consts
+
+object Link {
+
+  final val License = Link("https://github.com/stouma915/minesweeper-scala/blob/main/LICENSE")
+  final val Repository = Link("https://github.com/stouma915/minesweeper-scala")
+  final val GitHubPages = Link("https://pages.github.com")
+
+}
+
+case class Link(url: String)
---
+++ src/main/scala/net/st915/minesweeper/ui/consts/UIText.scala
@@ -0,0 +1,24 @@
+package net.st915.minesweeper.ui.consts
+
+object UIText {
+
+  final val ThisSiteIsLicensedUnderThe = UIText("This site is licensed under the ")
+  final val MITLicense = UIText("MIT License")
+  final val ThisSiteIsOpenSource = UIText("This site is open source. ")
+  final val ImproveThisSite = UIText("Improve this site")
+  final val PoweredBy = UIText("Powered by ")
+  final val GitHubPages = UIText("GitHub Pages")
+
+  final val DifficultiesColon = UIText("Difficulties:")
+
+  final val CurrentlyUnderDevelopment = UIText("Currently Under Development.")
+
+  final val RestartButton = UIText("Restart")
+  final val EnterFlagPlaceMode = UIText("Enter Flag Place Mode")
+  final val ExitFlagPlaceMode = UIText("Exit Flag Place Mode")
+
+  final val Period = UIText(".")
+
+}
+
+case class UIText(content: String)
