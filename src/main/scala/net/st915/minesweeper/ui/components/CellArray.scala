59b255ce5f1714507f7ed9279284b751fb38caa8
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-08 13:17:26 +0900

    Add CellArray

---
+++ src/main/scala/net/st915/minesweeper/ui/components/CellArray.scala
@@ -0,0 +1,40 @@
+package net.st915.minesweeper.ui.components
+
+import cats.effect.unsafe.IORuntime
+import cats.effect.Sync
+import net.st915.minesweeper.ui.consts.CSSClasses
+import net.st915.minesweeper.ui.components.instances.*
+import net.st915.minesweeper.ui.components.typeclasses.*
+import net.st915.minesweeper.{Coordinate, Difficulty}
+import org.scalajs.dom.*
+
+object CellArray {
+
+  import cats.syntax.flatMap.*
+  import cats.syntax.functor.*
+  import cats.syntax.traverse.*
+
+  def wired[F[_]: Sync](diff: Difficulty)(using HTMLDocument, IORuntime): F[HTMLDivElement] = {
+    given CanAppendElement[F] = SyncCanAppendElement[F]
+    given CanCreateElement[F, HTMLDivElement] = MonadCanCreateElementDiv[F]
+    given CanUpdateElementClass[F] = SyncCanUpdateElementClass[F]
+
+    for {
+      containerDiv <- CanCreateElement[F, HTMLDivElement].create
+      _ <- CanUpdateElementClass[F].perform(containerDiv, CSSClasses.CellArray)
+
+      lines <- (0 until diff.height)
+        .toList
+        .map { y =>
+          CellLine.wired[F](y, diff)
+        }
+        .sequence
+      _ <- lines
+        .map { line =>
+          CanAppendElement[F].perform(containerDiv, line)
+        }
+        .sequence
+    } yield containerDiv
+  }
+
+}
