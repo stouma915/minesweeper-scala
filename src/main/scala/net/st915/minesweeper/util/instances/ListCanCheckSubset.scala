51f78f6ca686765edb9948fe48af7e61620202d5
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-12 19:24:28 +0900

    Update ListCanCheckSubset.scala

--- src/main/scala/net/st915/minesweeper/util/instances/ListCanCheckSubset.scala
+++ src/main/scala/net/st915/minesweeper/util/instances/ListCanCheckSubset.scala
@@ -5,6 +5,6 @@ import net.st915.minesweeper.util.typeclasses.CanCheckSubset
 object ListCanCheckSubset extends CanCheckSubset[List] {
 
   override def isSubset[A](a: List[A], b: List[A]): Boolean =
-    a.exists { x => b.contains(x) }
+    a.exists(b.contains)
 
 }
