a31ab2e594f9e61a02a08a9a19e4463acc48d617
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-13 15:21:07 +0900

    Update Minesweeper.scala

--- src/main/scala/net/st915/minesweeper/Minesweeper.scala
+++ src/main/scala/net/st915/minesweeper/Minesweeper.scala
@@ -1,24 +1,26 @@
 package net.st915.minesweeper
 
-import cats.effect.IO
+import cats.effect.*
 import cats.effect.unsafe.IORuntime
 import net.st915.minesweeper.beforeui.BeforeUI
 import net.st915.minesweeper.eventloop.EventLoop
 import net.st915.minesweeper.ui.RenderUI
 import org.scalajs.dom.*
 
-@main def main(): Unit = {
-
+object Minesweeper extends IOApp {
+  
   given HTMLDocument = org.scalajs.dom.document
   given Window = org.scalajs.dom.window
   given IORuntime = cats.effect.unsafe.implicits.global
 
-  val program = for {
-    runContext <- BeforeUI.wired[IO]
-    _ <- RenderUI.wired[IO](runContext)
-    _ <- EventLoop.wired[IO](GameState.empty)
-  } yield ()
+  def run(args: List[String]): IO[ExitCode] = {
+    val program = for {
+      runContext <- BeforeUI.wired[IO]
+      _ <- RenderUI.wired[IO](runContext)
+      _ <- EventLoop.wired[IO](GameState.empty)
+    } yield ()
 
-  program.start.unsafeRunAndForget()
+    program.start >> IO(ExitCode.Success)
+  }
 
 }
