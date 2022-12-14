93ed42bbc022495fbe20dd6df5c864a7bc0b861e
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-14 18:26:02 +0900

    Add aliases

---
+++ immutable-scalajs/src/main/scala/net/st915/immutablescalajs/package.scala
@@ -0,0 +1,11 @@
+package net.st915
+
+package object immutablescalajs {
+
+  type ScalaJSAnchorElement = org.scalajs.dom.HTMLAnchorElement
+  type ScalaJSDivElement = org.scalajs.dom.HTMLDivElement
+  type ScalaJSH1Element = org.scalajs.dom.HTMLHeadingElement
+  type ScalaJSParagraphElement = org.scalajs.dom.HTMLParagraphElement
+  type ScalaJSSpanElement = org.scalajs.dom.HTMLSpanElement
+
+}
