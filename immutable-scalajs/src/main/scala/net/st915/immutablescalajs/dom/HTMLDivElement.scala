27308bf8030c57911e4ae1ec24728701e8ad1f1a
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-15 15:22:03 +0900

    Update HTMLDivElement.scala

--- immutable-scalajs/src/main/scala/net/st915/immutablescalajs/dom/HTMLDivElement.scala
+++ immutable-scalajs/src/main/scala/net/st915/immutablescalajs/dom/HTMLDivElement.scala
@@ -6,7 +6,7 @@ import net.st915.immutablescalajs.dom.properties.*
 
 final case class HTMLDivElement(
   override val cssClass: Option[CSSClass],
-  override val id: Option[ID],
+  override val id: Option[ID]
 ) extends HTMLElement(TagName.Div)
     with HasCSSClass
     with HasID
