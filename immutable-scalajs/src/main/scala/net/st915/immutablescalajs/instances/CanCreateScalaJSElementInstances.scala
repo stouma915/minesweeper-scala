24fe4c8da902e408447f8f6e468120c1c7b9cb6e
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-16 09:42:00 +0900

    Update CanCreateScalaJSElementInstances.scala

--- immutable-scalajs/src/main/scala/net/st915/immutablescalajs/instances/CanCreateScalaJSElementInstances.scala
+++ immutable-scalajs/src/main/scala/net/st915/immutablescalajs/instances/CanCreateScalaJSElementInstances.scala
@@ -7,7 +7,8 @@ import net.st915.immutablescalajs.typeclasses.CanCreateScalaJSElement
 
 trait CanCreateScalaJSElementInstances {
 
-  given monadCanCreateScalaJSElement[F[_]: Monad, A <: HTMLElement, B <: ScalaJSElement]: CanCreateScalaJSElement[F, A, B] with
+  given monadCanCreateScalaJSElement[F[_]: Monad, A <: HTMLElement, B <: ScalaJSElement]
+    : CanCreateScalaJSElement[F, A, B] with
     override def apply(element: A)(using ScalaJSDocument): F[B] =
       Monad[F].pure {
         summon[ScalaJSDocument]
