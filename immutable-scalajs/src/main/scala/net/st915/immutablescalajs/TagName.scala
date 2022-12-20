ad4c7637badbd8842b2c5cdbcfc8360f9e9054ee
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-20 12:11:32 +0900

    Update TagName

--- immutable-scalajs/src/main/scala/net/st915/immutablescalajs/TagName.scala
+++ immutable-scalajs/src/main/scala/net/st915/immutablescalajs/TagName.scala
@@ -2,16 +2,16 @@ package net.st915.immutablescalajs
 
 object TagName {
 
-  object Anchor extends TagName("a")
-  object BR extends TagName("br")
-  object Div extends TagName("div")
-  object H1 extends TagName("h1")
-  object Paragraph extends TagName("p")
-  object Span extends TagName("span")
+  final val Anchor = TagName("a")
+  final val BR = TagName("br")
+  final val Div = TagName("div")
+  final val H1 = TagName("h1")
+  final val Paragraph = TagName("p")
+  final val Span = TagName("span")
 
 }
 
-sealed class TagName private (raw: String) {
+final class TagName private (raw: String) {
 
   def unwrap: String = raw
 
