4013fb10369acf4913110dc8fb7574d3ca89b582
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-26 17:06:59 +0900

    Add Div

---
+++ typesafe-scalajs/src/main/scala/net/st915/typesafescalajs/elements/Div.scala
@@ -0,0 +1,28 @@
+package net.st915.typesafescalajs.elements
+
+import cats.Monoid
+import net.st915.typesafescalajs.Node
+import net.st915.typesafescalajs.elements.properties.*
+import net.st915.typesafescalajs.elements.typeclasses.CanCopyWithNewProperty
+
+object Div {
+
+  given canCopyDivWithNewClassName: CanCopyWithNewProperty[Div, ClassName] with
+    override def apply(newProperty: ClassName)(element: Div): Div =
+      element.copy(className = newProperty)
+
+  given canCopyDivWithNewID: CanCopyWithNewProperty[Div, ID] with
+    override def apply(newProperty: ID)(element: Div): Div =
+      element.copy(id = newProperty)
+
+  given canCopyDivWithNewChilds: CanCopyWithNewProperty[Div, Childs] with
+    override def apply(newProperty: Childs)(element: Div): Div =
+      element.copy(childs = newProperty)
+
+}
+
+case class Div(
+  override val className: ClassName = Monoid[ClassName].empty,
+  override val id: ID = Monoid[ID].empty,
+  override val childs: Childs = Monoid[Childs].empty
+) extends Element
