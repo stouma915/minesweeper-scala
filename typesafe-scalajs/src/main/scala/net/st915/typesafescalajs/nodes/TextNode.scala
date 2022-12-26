e08bfc8c05bd83dc24b19abe7f00b190805f116c
stouma915 <sasakitouma@yahoo.ne.jp>
2022-12-26 17:58:58 +0900

    Refactor nodes

--- typesafe-scalajs/src/main/scala/net/st915/typesafescalajs/nodes/TextNode.scala
+++ typesafe-scalajs/src/main/scala/net/st915/typesafescalajs/nodes/TextNode.scala
@@ -3,17 +3,14 @@ package net.st915.typesafescalajs.nodes
 import cats.Monoid
 import net.st915.typesafescalajs.Node
 import net.st915.typesafescalajs.nodes.attributes.HasInnerText
+import net.st915.typesafescalajs.nodes.copyables.CanCopyWithNewInnerText
 import net.st915.typesafescalajs.nodes.properties.InnerText
-import net.st915.typesafescalajs.typeclasses.CanCopyWithNewProperty
-
-object TextNode {
-
-  given canCopyTextNodeWithNewInnerText: CanCopyWithNewProperty[TextNode, InnerText] with
-    override def apply(newProperty: InnerText)(element: TextNode): TextNode =
-      element.copy(innerText = newProperty)
-
-}
 
 case class TextNode(
   override val innerText: InnerText = Monoid[InnerText].empty
-) extends Node with HasInnerText
+) extends Node with HasInnerText with CanCopyWithNewInnerText[TextNode] {
+
+  override def copyWith(newProperty: InnerText): TextNode =
+    copy(innerText = newProperty)
+
+}
